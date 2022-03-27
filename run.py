d8b41390994837f505f324f15a3420f1bb4f8d50
John Gleason <jogleasonjr@gmail.com>
2022-03-27 12:39:30 -0500

    Switch to JSON publishing

--- run.py
+++ run.py
@@ -4,37 +4,41 @@ import mido
 import paho.mqtt.client as mqtt
 import struct
 
+
+# TODO: Add as startup args
+mqtt_server = "mqtt.jtpk.io"
+mqtt_port = 1883
+mqtt_topic = "midi/pk"
+midi_attached_device = "padKONTROL:padKONTROL MIDI 2 20:1"
+
+print("Discovered MIDI devices:")
 print(mido.get_output_names())
 
-# The callback for when the client receives a CONNACK response from the server.
+# Callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
-    # client.subscribe("$SYS/#")
 
+# Callback for messages sent to the topic
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic + " " + str(msg.payload))
+
+
+# Create an object from the raw midi message and publish serialized JSON to the topic
+def publish_to_mqtt_topic(message):
+    kvps = ("event=" + str(message)).split(" ")
+    json = {k: v for kvp in kvps for k, v in (kvp.split("="),)}
+    json_str = str(json).replace("'", '"')
+    client.publish(mqtt_topic, payload=json_str)
+    print(json_str)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.connect(mqtt_server, mqtt_port, 60)
 
-client.connect("mqtt.jtpk.io", 1883, 60)
-
-def print_message(message):
-    b = message.bytes()
-    ba = bytearray(b)
-    client.publish("midi/pk/raw", payload=ba)
-    print(message)
-
-port = mido.open_input('padKONTROL:padKONTROL MIDI 2 20:1', callback=print_message)
-port.callback = print_message
+port = mido.open_input(midi_attached_device)
+port.callback = publish_to_mqtt_topic
 
 client.loop_forever()
-
-
-
-
-# control_change channel=9 control=21 value=19 time=0
-# note_on channel=9 note=56 velocity=127 time=0
-
